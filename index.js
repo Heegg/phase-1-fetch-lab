1214958b19d32bb1fe33b10a45b7d8cda3e3312a
Heeji Hyun <wlgmlgus@gmail.com>
2023-01-23 19:51:33 -0500

    commit tree c91baf3e3b827ae17a770233da94cb26570afd1f

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json())
+    .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
@@ -12,6 +17,11 @@ function renderBooks(books) {
   });
 }
 
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+fetch("")
+
+
